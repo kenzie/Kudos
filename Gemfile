1f1cb991c5ecaf8b71af012fc9f3d52c4d6209a9
Kenzie Campbell <kenzie@route19.com>
2012-11-21 13:32:56 -0400

    Added basic Twitter oAuth

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '1.9.3'
 gem 'sinatra'
 gem 'haml'
 gem 'tweetstream'
+gem 'oauth'
+gem 'json'
 
 group :development, :test do
   gem 'sinatra-reloader'
--- service.rb
+++ service.rb
@@ -2,6 +2,8 @@
 
 configure do
   set :haml, { :format => :html5 }
+  TWITTER = YAML.load_file(File.expand_path("../config/twitter.yml", __FILE__)) unless defined? TWITTER
+  use Rack::Session::Pool, :expire_after => 2592000
 end
 
 configure(:development) do |c|
@@ -12,4 +14,30 @@ end
 get '/' do
   expires 3600, :public
   haml :index
+end
+
+get '/auth/twitter' do
+  puts "Redirect URL: #{redirect_uri}"
+  request_token = client.get_request_token(:oauth_callback => redirect_uri)
+  session[:request_token] = request_token
+  redirect request_token.authorize_url(:oauth_callback => redirect_uri)
+end
+
+get '/auth/twitter/callback' do
+  access_token = session[:request_token].get_access_token
+  screen_name = access_token.params[:screen_name]
+  oauth_token = access_token.params[:oauth_token]
+  oauth_token_secret = access_token.params[:oauth_token_secret]
+  "Screen Name: #{screen_name} <br/> Access Token: #{oauth_token} <br/> Access Token Secret: #{oauth_token_secret}"
+end
+
+def client
+  OAuth::Consumer.new( TWITTER['consumer_key'], TWITTER['consumer_secret'], :site => 'https://api.twitter.com' )
+end
+
+def redirect_uri
+  uri = URI.parse(request.url)
+  uri.path = '/auth/twitter/callback'
+  uri.query = nil
+  uri.to_s
 end
